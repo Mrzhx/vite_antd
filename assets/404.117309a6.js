ec919f14686a2201d617f6a3dc94fc67a6a76a20
Mrzhx <Mrzhx@users.noreply.github.com>
2022-04-15 02:07:56 +0000

    deploy: fa6ffa810ab130597309cc88fbfef6da5e8ea9cb

---
+++ assets/404.117309a6.js
@@ -0,0 +1,1 @@
+import"./index.9d359c96.js";import{B as o}from"./button.979918b3.js";import{R as s}from"./index.685281fd.js";import{k as t,m as e}from"./react-venders.18930c37.js";const p=()=>t(s,{status:"404",title:"404",subTitle:"Sorry, the page you visited does not exist.",extra:t(o,{type:"primary",onClick:()=>e.push("/"),children:"Back Home"})});export{p as default};
---
+++ assets/RootLayout.c77b6a2c.js
@@ -0,0 +1,1 @@
+import{k as o,F as a}from"./react-venders.18930c37.js";const s=({children:t})=>o(a,{children:t});export{s as default};
